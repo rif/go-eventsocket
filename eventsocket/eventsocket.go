0cdee3fe2d8ec3d7dbd96aaac653277a6f07487d
Radu Ioan Fericean <radu@fericean.ro>
2020-07-07 15:44:54 +0300

    Sending eror rather than closing

--- eventsocket/eventsocket.go
+++ eventsocket/eventsocket.go
@@ -269,7 +269,7 @@ func (h *Connection) RemoteAddr() net.Addr {
 
 // Close terminates the connection.
 func (h *Connection) Close() {
-	close(h.err)
+	h.err <- errors.New("closing")
 	h.conn.Close()
 }
 
